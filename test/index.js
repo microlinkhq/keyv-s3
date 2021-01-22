9bb21e1f9c833adf68dee6ad37369ba847869f3c
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-01-22 09:27:55 +0000

    feat: use aws-sdk/client-s3 package
    it reduces require footprint

--- test/index.js
+++ test/index.js
@@ -1,17 +1,10 @@
-const AWS = require('aws-sdk')
-
 const KeyvS3 = require('..')
 
-const {
-  S3_BUCKET_NAME,
-  S3_ACCESS_KEY_ID,
-  S3_SECRET_ACCESS_KEY,
-  S3_ENDPOINT
-} = process.env
+const { S3_BUCKET_NAME, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } = process.env
 
 module.exports = new KeyvS3({
+  region: 'us-east-1',
   namespace: S3_BUCKET_NAME,
-  endpoint: new AWS.Endpoint(S3_ENDPOINT),
   accessKeyId: S3_ACCESS_KEY_ID,
   secretAccessKey: S3_SECRET_ACCESS_KEY
 })
