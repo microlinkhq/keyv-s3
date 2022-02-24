8eaf03c6ff02cba4f108e0f17b35ede241c897c8
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-02-24 15:55:40 +0000

    test: setup s3client

--- test/util.js
+++ test/util.js
@@ -1,10 +1,19 @@
+const { S3Client } = require('@aws-sdk/client-s3')
 const KeyvS3 = require('..')
 
-const { S3_BUCKET_NAME, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } = process.env
+const {
+  S3_REGION,
+  S3_BUCKET_NAME,
+  S3_ACCESS_KEY_ID,
+  S3_SECRET_ACCESS_KEY
+} = process.env
 
-module.exports = new KeyvS3({
-  region: 'us-east-1',
-  namespace: S3_BUCKET_NAME,
-  accessKeyId: S3_ACCESS_KEY_ID,
-  secretAccessKey: S3_SECRET_ACCESS_KEY
+const s3client = new S3Client({
+  region: S3_REGION,
+  credentials: {
+    accessKeyId: S3_ACCESS_KEY_ID,
+    secretAccessKey: S3_SECRET_ACCESS_KEY
+  }
 })
+
+module.exports = new KeyvS3({ namespace: S3_BUCKET_NAME, s3client })
